dabe67c0f3225d758186ccb5a07e421dd8c789d4
MJLawlor <ml28@sanger.ac.uk>
2016-05-04 14:09:38 +0100

    Update lookuptable.py

--- generic/lookuptable.py
+++ generic/lookuptable.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding=utf-8
 # author:       Máire Ní Leathlobhair (ml677@cam.ac.uk)
 # date:         May 2016
 # description:  Filters tumour variants against matched normal based on variant allele fraction (VAF). 
@@ -8,6 +9,14 @@
 
 import sys
 import os
+
+if len(sys.argv) != 3:
+    print '\nlookuptable.py: Compare tumour variant list to matched normal variant list'
+    print '                  Variants arising due to putative contamination from host or matched normal are removed'
+    print '            '              
+    print '                    Input: Tumour and host variant lists.'
+    print '                    Usage: lookuptable.py /tumour/variant/list.txt /host/variant/list\n'
+    sys.exit(0)
 		
 file1=open(sys.argv[1], 'r+') ##tumour variant list
 file2=open(sys.argv[2], 'r+') ##host variant list
