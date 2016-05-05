9d37e325aad9f5078ff54da4363a97d16aac81f6
MaireNiL <ml28@sanger.ac.uk>
2016-05-05 11:30:15 +0100

    Update vaf.R

--- generic/vaf.R
+++ generic/vaf.R
@@ -1,3 +1,6 @@
+# mitotypus data post-processing pipeline: R script for generating VAF plots
+# author:       Máire Ní Leathlobhair (ml677@cam.ac.uk)
+# date:         May 2016
 
 args <- commandArgs(TRUE)
 allData <- read.table(args[1], header=T, sep="")
@@ -6,8 +9,10 @@ pdf(args[3], useDingbats=FALSE)
 cutoff <- as.numeric(args[4])
 par(mar=c(5.1, 9.1, 5.1, 5.1), xpd=TRUE)
 
+# plot all substitutions
 plot(allData$POS, allData$VAF, xlab="", main="", ylab="", col="gray", cex=1.5, pch=19, xlim=c(0,17000), ylim=c(0,1), yaxt="n",bty='l')
 par(new=TRUE)
+# plot only unfiltered substitutions
 plot(vafData$POS, vafData$VAF, xlab="", main=args[5], ylab="", col="black", cex=1.5, pch=19, xlim=c(0,17000), ylim=c(0,1), yaxt="n",bty='l')
 # label tumour and host VAF bands
 tumour_line = median(vafData$VAF)
@@ -18,8 +23,10 @@ if (grepl("T",args[4])){
 	text(19000,host_line,labels = 'Host line')
 }
 segments(-40,tumour_line,17000,tumour_line,col="lightgrey",lty=c(1),lwd=1,lend="square")
+# label VAF cutoff
 text(19000,cutoff,labels = 'VAF Cutoff')
-axis(2, at = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
 segments(-40, cutoff, 17000, cutoff, col="black", lty=c(1), lwd=1, lend="square")
-legend("topleft", inset=c(-.45,0),c("Host contamination","Tumour Variant"), pch=c(18, 18), col=c("gray", "black",bty="n") 
-
+# set y-axis intervals
+axis(2, at = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
+# add legend
+legend("topleft", inset=c(-.45,0),c("Variant","Contamination"), pch=c(19, 19), col=c("black", "gray"),bty="n") 
