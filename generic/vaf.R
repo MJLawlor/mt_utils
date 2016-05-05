56a9c66dbe34dec5d1b4f2cdf12740ded7926329
MaireNiL <ml28@sanger.ac.uk>
2016-05-05 11:07:22 +0100

    Update vaf.R

--- generic/vaf.R
+++ generic/vaf.R
@@ -9,9 +9,14 @@ par(mar=c(5.1, 9.1, 5.1, 5.1), xpd=TRUE)
 plot(allData$POS, allData$VAF, xlab="", main="", ylab="", col="gray", cex=1.5, pch=19, xlim=c(0,17000), ylim=c(0,1), yaxt="n",bty='l')
 par(new=TRUE)
 plot(vafData$POS, vafData$VAF, xlab="", main=args[5], ylab="", col="black", cex=1.5, pch=19, xlim=c(0,17000), ylim=c(0,1), yaxt="n",bty='l')
+# label tumour and host VAF bands
 tumour_line = median(vafData$VAF)
 host_line = median(vafData$VAF)
-
+if (grepl("T",args[4])){	
+	text(19000,tumour_line,labels = 'Tumour line')
+} else {
+	text(19000,host_line,labels = 'Host line')
+}
 segments(-40,tumour_line,17000,tumour_line,col="lightgrey",lty=c(1),lwd=1,lend="square")
 text(19000,cutoff,labels = 'VAF Cutoff')
 axis(2, at = c(0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1))
